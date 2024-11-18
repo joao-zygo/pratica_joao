24ada393c5a923f28fda0d2aa98aa12aeeed8d8c
JOÃO VITOR MARQUETTI BENTO <joao-vitor_bento@estudante.sesisenai.org.br>
2024-11-18 08:24:25 -0300

    adicionar create do cliente

---
+++ cliente/create.php
@@ -0,0 +1,32 @@
+<?php
+    include "../db.php";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['nome']) && isset($_POST['email'])) {
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $telefone = $_POST['telefone'];
+            $conn->query("INSERT INTO cliente (nome, email,telefone) VALUES ('$nome', '$email', '$telefone')");
+            
+        }
+    }
+    $conn->close();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create</title>
+</head>
+<body>
+    <form method="POST">
+        <label for="name">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="email" required>
+        <label for="telefone">Telefone:</label>
+        <input type="tel" id="telefone" name="telefone" required>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
